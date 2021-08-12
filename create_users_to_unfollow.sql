28ec7de502fb3ee78adca859a4f6c523edc3096b
ade <adrian.harrison.uk@gmail.com>
2021-08-12 19:01:49 +0100

    cleaned version wip4

--- create_users_to_unfollow.sql
+++ create_users_to_unfollow.sql
@@ -31,17 +31,5 @@ WHERE
 	FROM
 		ignore_users iu);
 
-delete from friend fr where fr.screen_name = 'nowokehere';
-delete from friend fr where fr.screen_name = 'Jack10167709';
-delete from friend fr where fr.screen_name = 'IanParker24';
-delete from friend fr where fr.screen_name = 'Sadie200009';
-delete from friend fr where fr.screen_name = '_Kate_Barber';
-delete from friend fr where fr.screen_name = 'KevinAr63351894';
-delete from friend fr where fr.screen_name = 'mrs_valerie_fi2';
-delete from friend fr where fr.screen_name = 'PompeyKimmy';
-delete from friend fr where fr.screen_name = 'Joey51574402';
--- Create table of twitter ids Who I follow and follows me - 4105
---drop table if exists following_and_follow_me cascade;
---create table following_and_follow_me as 
---SELECT fo.twitter_id, fr.name from follower fo 
---inner join friend fr on fo.twitter_id = fr.twitter_id;
+--	run when friends unfollowed in Twitter
+--delete from friend fr where fr.screen_name in (select utu.screen_name from users_to_unfollow utu);
